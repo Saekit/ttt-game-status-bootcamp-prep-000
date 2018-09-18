0fb9edbcde507ed877d331a4cadbdbf8282bb77a
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 16:39:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,8 +47,14 @@ WIN_COMBINATIONS = [
 # end
 
 def won?(board) WIN_COMBINATIONS.each do |wincombo| 
-  if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" return wincombo elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
-    return wincombo end end false end
+  if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+      return wincombo 
+  elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
+    return wincombo 
+    end 
+  end 
+  false 
+end
 
 
 
