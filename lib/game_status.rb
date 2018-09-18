1d3a715a1cbcc6fec090cd9b53f4fa1080319336
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 16:24:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,50 +15,50 @@ WIN_COMBINATIONS = [
   [2,4,6]   # diag right to left
 ]
 
-def won?(board) 
+# def won?(board) 
 
-  WIN_COMBINATIONS.detect do |win_combos|
-    win_index_1 = win_combos[0]
-    win_index_2 = win_combos[1]
-    win_index_3 = win_combos[2]
+#   WIN_COMBINATIONS.detect do |win_combos|
+#     win_index_1 = win_combos[0]
+#     win_index_2 = win_combos[1]
+#     win_index_3 = win_combos[2]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+#     position_1 = board[win_index_1]
+#     position_2 = board[win_index_2]
+#     position_3 = board[win_index_3]
     
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combos
+#     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+#       return win_combos
   
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combos
+#     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+#       return win_combos
       
-    else
-      return false
-    end
-  end
-  WIN_COMBINATIONS.detect do |win_combos|
-    win_index_4 = win_combos[3]
-    win_index_5 = win_combos[4]
-    win_index_6 = win_combos[5]
+#     else
+#       return false
+#     end
+#   end
+#   WIN_COMBINATIONS.detect do |win_combos|
+#     win_index_4 = win_combos[3]
+#     win_index_5 = win_combos[4]
+#     win_index_6 = win_combos[5]
     
-    position_4 = board[win_index_4]
-    position_5 = board[win_index_5]
-    position_6 = board[win_index_6]
+#     position_4 = board[win_index_4]
+#     position_5 = board[win_index_5]
+#     position_6 = board[win_index_6]
     
-    if position_4 == "X" && position_5 == "X" && position_6 == "X"
-      return [3,4,5]
+#     if position_4 == "X" && position_5 == "X" && position_6 == "X"
+#       return [3,4,5]
   
-    elsif position_4 == "O" && position_5 == "O" && position_6 == "O"
-      return [3,4,5]
+#     elsif position_4 == "O" && position_5 == "O" && position_6 == "O"
+#       return [3,4,5]
       
-    else
-      return false
-    end
-  end
+#     else
+#       return false
+#     end
+#   end
     
   
   
-end
+# end
 
 
 def full?(board)
