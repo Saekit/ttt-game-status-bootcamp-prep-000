ca8ad6325b9055c15f274f93b8567da8af8d7bbb
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 15:45:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,18 +37,18 @@ def won?(board)
     end
   end
   WIN_COMBINATIONS.detect do |win_combos|
-    win_index_1 = win_combos[3]
-    win_index_2 = win_combos[4]
-    win_index_3 = win_combos[5]
+    win_index_4 = win_combos[3]
+    win_index_5 = win_combos[4]
+    win_index_6 = win_combos[5]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_4 = board[win_index_4]
+    position_5 = board[win_index_5]
+    position_6 = board[win_index_6]
     
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if position_4 == "X" && position_5 == "X" && position_6 == "X"
       return win_combos
   
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_4 == "O" && position_2 == "O" && position_3 == "O"
       return win_combos
       
     else
