c1dfc45350cd243d75dec3cf3aa1fc01e6d5978a
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 15:19:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,8 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combos
   
-    elsif 
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combos
 
 end
 
